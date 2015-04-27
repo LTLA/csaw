a20ec71882ed54d24ebefd540d79d0db573138fa
Aaron Lun <alun@wehi.EDU.AU>
2015-04-27 00:21:42 +0000

    Added clusterFDR, minor edits to other functions.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/csaw@103108 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SEmethods.R
+++ R/SEmethods.R
@@ -1,19 +1,39 @@
 # This defines some wrapper functions for various statistical methods 
 # in edgeR, for the SummarizedExperiment class. The idea is to relieve
 # the need for manual specification of the inputs every time; especially
-# for the library sizes.
+# for the library sizes (some care is required to still allow that).
 #
 # written by Aaron Lun
 # created 2 September 2014
+# last modified 26 April 2015
 
 setMethod("normalize", "SummarizedExperiment", function(object, ...) {
-	if (is.null(object$totals)) { 
-		return(normalizeCounts(counts=assay(object), ...))
+	opt <- match.call(normalizeCounts, do.call(call, list("normalizeCounts", 
+		counts=assay(object), ...)))
+
+	if (is.null(opt$lib.sizes)) { 
+		if (is.null(object$totals)) {
+			warning("library sizes not found in 'totals'")
+		} else {
+			opt$lib.sizes <- object$totals
+		}
 	}
-	normalizeCounts(counts=assay(object), lib.sizes=object$totals, ...)
+
+	eval(opt)
 })
 
 setGeneric("asDGEList", function(object, ...) { standardGeneric("asDGEList") })
 setMethod("asDGEList", "SummarizedExperiment", function(object, ...) {
-	DGEList(assay(object), lib.size=object$totals, ...)
+	opt <- match.call(DGEList, do.call(call, list("DGEList", 
+		counts=assay(object), ...)))
+	
+	if (is.null(opt$lib.size)) { 
+		if (is.null(object$totals)) { 
+			warning("library sizes not found in 'totals'")	
+		} else {
+			opt$lib.size <- object$totals
+		}
+	}
+	
+	eval(opt)
 })
