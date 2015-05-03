2566dacb8571977ace2c05694264bdf1d2edbcc6
Aaron Lun <alun@wehi.EDU.AU>
2015-05-03 01:49:03 +0000

    Added checkBimodality() function for, well, checking bimodality.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/csaw@103394 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/checkBimodality.R
@@ -0,0 +1,65 @@
+checkBimodality <- function(bam.files, regions, width=100, param=readParam(), prior.count=2) 
+# This gives the maximum strand bimodality score for any base pair in each
+# region. The idea is to try to distinguish between genuine TF binding sites
+# and read stacks or other artifacts.
+#
+# written by Aaron Lun
+# created 1 May 2015
+{
+	nbam <- length(bam.files)
+	paramlist <- .makeParamList(nbam, param)
+	extracted.chrs <- .activeChrs(bam.files, paramlist[[1]]$restrict)
+	ext.data <- .collateExt(nbam, width) 
+
+	totals <- integer(nbam)
+	nx <- length(regions)
+	out.scores <- rep(NA, nx)
+	indices <- split(1:nx, seqnames(regions))
+	prior.count <- as.double(prior.count)
+
+	for (chr in names(extracted.chrs)) {
+		chosen <- indices[[chr]]
+		outlen <- extracted.chrs[[chr]]
+		where <- GRanges(chr, IRanges(1, outlen))
+
+		# Pulling out reads as previously described.
+		collected <- list()
+		for (bf in 1:nbam) {
+			curpar <- paramlist[[bf]]
+			if (curpar$pe=="both") { 
+				stop("bimodality checking not supported for paired-end mode") 
+			} else if (curpar$pe=="none") {
+				reads <- .extractSE(bam.files[bf], where=where, param=curpar)
+			} else {
+				reads <- .extractBrokenPE(bam.files[bf], where=where, param=curpar)
+			}
+			
+			is.forward <- as.integer(reads$strand=="+")
+			left.pos <- reads$pos
+			right.pos <- reads$pos + reads$qwidth - 1L 
+
+			if (is.unsorted(left.pos)) { 
+				o <- order(left.pos)
+				left.pos <- left.pos[o]
+				right.pos <- right.pos[o]
+				is.forward <- is.forward[o]
+			}
+
+			curwidth <- ext.data$ext[bf]
+			if (curwidth < 1L || !is.finite(curwidth)) { stop('width must be a non-negative integer') }
+			collected[[bf]] <- list(left.pos, right.pos, is.forward, curwidth)
+		}
+
+		# Checking region order.
+		rstarts <- start(regions)[chosen]
+		rends <- end(regions)[chosen]
+		ro <- order(rstarts)
+		
+		# Computing bimodality scores.
+		out <- .Call(cxx_check_bimodality, collected, rstarts[ro], rends[ro], prior.count)
+		if (is.character(out)) { stop(out) }
+		out.scores[chosen][ro] <- out 
+	}
+
+	return(out.scores)	
+}
