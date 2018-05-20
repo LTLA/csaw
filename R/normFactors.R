37d201bf5ae15e977192fd6c2f5baa841d6a7365
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-20 15:25:15 +0100

    Removed confusing lib.size= option in norm* functions.

--- R/normFactors.R
+++ R/normFactors.R
@@ -1,4 +1,4 @@
-normFactors <- function(object, lib.sizes=NULL, weighted=FALSE, ..., assay.id="counts", se.out=TRUE) 
+normFactors <- function(object, weighted=FALSE, ..., assay.id="counts", se.out=TRUE) 
 # This provides a wrapper to perform TMM normalization with non-standard
 # library sizes (e.g. due to filtering) and weighting turned off.
 # Alternatively, it can do a form a fast loess-like normalization which uses
@@ -8,19 +8,24 @@ normFactors <- function(object, lib.sizes=NULL, weighted=FALSE, ..., assay.id="c
 # written by Aaron Lun
 # created 18 May 2018
 {
-    lib.sizes <- object$totals 
-    if (is.null(lib.sizes)) { 
-        stop("missing 'totals' from SummarizedExperiment 'colData'")
-    } 
+    lib.sizes <- object$totals
+    if (is.null(lib.sizes)) {
+        stop("library sizes not present in 'object$totals'")
+    }
 
 	out <- calcNormFactors(assay(object, i=assay.id, withDimnames=FALSE), 
         lib.size=lib.sizes, doWeighting=weighted, ...)
 
 	# Choosing to put these values in a different location, if requested. 
     if (!is.logical(se.out)) { 
+        if (ncol(object)!=ncol(se.out)) {
+            stop("number of libraries differs between 'se.out' and 'object'")
+        }
+
         if (!identical(se.out$totals, lib.sizes)) {
             stop("library sizes of 'se.out' and 'object' are not identical")
         }
+
         object <- se.out
         se.out <- TRUE
     }
--- R/normOffsets.R
+++ R/normOffsets.R
@@ -3,7 +3,7 @@
 #' @importFrom edgeR aveLogCPM
 #' @importFrom methods is
 #' @importFrom SummarizedExperiment assay assay<-
-normOffsets <- function(object, lib.sizes=NULL, type=c("scaling", "loess"), ..., assay.id="counts", se.out=TRUE) 
+normOffsets <- function(object, type=c("scaling", "loess"), ..., assay.id="counts", se.out=TRUE) 
 # Perform a fast loess normalization which uses the average count as the covariate, 
 # rather than the typical A-value-based methods to avoid instability at low abundances.
 #
@@ -17,14 +17,13 @@ normOffsets <- function(object, lib.sizes=NULL, type=c("scaling", "loess"), ...,
 	} 
 
     mat <- assay(object, i=assay.id, withDimnames=FALSE)
-    if (is.null(lib.sizes)) {
-        lib.sizes <- object$totals
-        if (is.null(lib.sizes)) {
-            stop("library sizes not present in 'object$totals'")
-        }
-    }
-    nlibs <- length(lib.sizes)
+    nlibs <- ncol(mat)
     nwin <- nrow(mat)
+
+    lib.sizes <- object$totals
+    if (is.null(lib.sizes)) {
+        stop("library sizes not present in 'object$totals'")
+    }
 
     # Scaled corrections squeeze offsets towards relative log-library sizes.
     # Constant value of `cont.cor' would squeeze them towards zero, which could be misleading.
@@ -37,24 +36,19 @@ normOffsets <- function(object, lib.sizes=NULL, type=c("scaling", "loess"), ...,
     # Computing the output mean.
     mean.out <- NULL
     if (is(se.out, "SummarizedExperiment")) { 
-        object <- se.out
-        se.out <- TRUE
-        
-        nwin <- nrow(object)
-        if (ncol(object)!=nlibs) {
+        if (ncol(se.out)!=nlibs) {
             stop("number of libraries differs between 'se.out' and 'object'")
         }
-
-        se.lib.sizes <- object$totals
-        if (is.null(se.lib.sizes)) {
-            stop("library sizes not present in 'se.out$totals'")
-        }
-        if (!identical(se.lib.sizes, lib.sizes)) {
-            stop("library sizes should be identical between 'se.out' and 'object'")
+        if (!identical(se.out$totals, lib.sizes)) {
+            stop("library sizes of 'se.out' and 'object' are not identical")
         }
 
-        mat2 <- assay(object, i=assay.id, withDimnames=FALSE)
+        mat2 <- assay(se.out, i=assay.id, withDimnames=FALSE)
         mean.out <- aveLogCPM(mat2, lib.size=lib.sizes, prior.count=cont.cor)/log2(exp(1))
+
+        object <- se.out
+        se.out <- TRUE
+        nwin <- nrow(object)
     }
     
     # Computing the offsets as log-differences from the average (implicit if the same average is used across libraries).
