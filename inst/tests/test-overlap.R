fbf6f0f800d5579127686e7dbe846b8a91d025cd
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-27 19:28:29 +0100

    Converted overlap tests to use testthat.

--- inst/tests/test-overlap.R
+++ inst/tests/test-overlap.R
@@ -1,176 +1,136 @@
 # This checks the overlap summarization functions, relative to the expected values.
+# library(testthat); library(csaw); source("test-overlap.R")
 
-suppressWarnings(suppressPackageStartupMessages(require(csaw)))
 source("simsam.R")
-
-####################################################################################################
-
-compcombine <- function(ranges, windows) {
-	olap <- findOverlaps(ranges, windows)
-	ns <- length(windows)
-	tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
-
-	# Straight-up comparison to combineTests, after discarding all NA's.
-	output <- combineOverlaps(olap, tab)
-	refstats <- combineTests(queryHits(olap), tab[subjectHits(olap),])
-	if (!identical(data.frame(output[!is.na(output$PValue),]), refstats)) { stop("mismatch in stats from near-identical calls!") }
-
-	# Testing with weights.
-	test.weight <- runif(ns)
-	output <- combineOverlaps(olap, tab, i.weight=test.weight)
-	refstats <- combineTests(queryHits(olap), tab[subjectHits(olap),], weight=test.weight[subjectHits(olap)])
-	if (!identical(data.frame(output[!is.na(output$PValue),]), refstats)) { stop("mismatch in stats from near-identical calls!") }
-
-	# More weight testing, where o.weight is constructed from the weight for each i.weight.
-	output2 <- combineOverlaps(olap, tab, o.weight=test.weight[subjectHits(olap)])
-	if (!identical(output, output2)) { stop("mismatch in stats from near-identical calls!") }
-
-	return(head(output))
-}
-
-set.seed(34823)
-
 chromos <- c(A=1000, B=2000)
 
-regions <- generateWindows(chromos, 10, 500)
-windows <- generateWindows(chromos, 100, 50)
-compcombine(regions, windows)
+set.seed(130000)
+test_that("combineOverlaps works correctly", {
+    for (nreg in c(2, 10)) {
+        for (nwin in c(1, 10, 100)) {
+            regions <- generateWindows(chromos, nreg, 500)
+            windows <- generateWindows(chromos, nwin, 50)
 
-regions <- generateWindows(chromos, 10, 500)
-windows <- generateWindows(chromos, 200, 20)
-compcombine(regions, windows)
+            olap <- findOverlaps(regions, windows)
+            ns <- length(windows)
+            tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
 
-regions <- generateWindows(chromos, 2, 500)
-windows <- generateWindows(chromos, 100, 50)
-compcombine(regions, windows)
+            # Straight-up comparison to combineTests, after discarding all NA's.
+            output <- combineOverlaps(olap, tab)
+            refstats <- combineTests(queryHits(olap), tab[subjectHits(olap),])
+            expect_identical(output[!is.na(output$PValue),], refstats)
 
-####################################################################################################
+        	# Testing with weights.
+            test.weight <- runif(ns)
+            output <- combineOverlaps(olap, tab, i.weight=test.weight)
+            refstats <- combineTests(queryHits(olap), tab[subjectHits(olap),], weight=test.weight[subjectHits(olap)])
+            expect_identical(output[!is.na(output$PValue),], refstats)
 
-compbest <- function(ranges, windows) {
-	olap <- findOverlaps(ranges, windows)
-	ns <- length(windows)
-	tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
+        	# More weight testing, where o.weight is constructed from the weight for each i.weight.
+            output2 <- combineOverlaps(olap, tab, o.weight=test.weight[subjectHits(olap)])
+            expect_identical(output, output2)
+        }
+    }
+})
 
-	output <- getBestOverlaps(olap, tab)
-	refstats <- getBestTest(queryHits(olap), tab[subjectHits(olap),])
-	refstats$best <- subjectHits(olap)[refstats$best]
-	if (!identical(data.frame(output[!is.na(output$PValue),]), refstats)) { stop("mismatch in stats from near-identical calls!") }
+set.seed(130001)
+test_that("getBestOverlaps works correctly", {
+    for (nreg in c(2, 10)) {
+        for (nwin in c(1, 10, 100)) {
+            regions <- generateWindows(chromos, nreg, 500)
+            windows <- generateWindows(chromos, nwin, 50)
+            
+        	olap <- findOverlaps(regions, windows)
+        	ns <- length(windows)
+        	tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
+        
+        	output <- getBestOverlaps(olap, tab)
+        	refstats <- getBestTest(queryHits(olap), tab[subjectHits(olap),])
+        	refstats$best <- subjectHits(olap)[refstats$best]
+        	expect_identical(output[!is.na(output$PValue),], refstats)
+        
+        	# Testing with weights.
+        	test.weight <- runif(ns)
+        	output <- getBestOverlaps(olap, tab, i.weight=test.weight)
+        	refstats <- getBestTest(queryHits(olap), tab[subjectHits(olap),], weight=test.weight[subjectHits(olap)])
+        	refstats$best <- subjectHits(olap)[refstats$best]
+        	expect_identical(output[!is.na(output$PValue),], refstats)
+        
+        	# More weight testing.
+        	output2 <- getBestOverlaps(olap, tab, o.weight=test.weight[subjectHits(olap)])
+        	expect_identical(output, output2)
+        }
+    }
+})
 
-	# Testing with weights.
-	test.weight <- runif(ns)
-	output <- getBestOverlaps(olap, tab, i.weight=test.weight)
-	refstats <- getBestTest(queryHits(olap), tab[subjectHits(olap),], weight=test.weight[subjectHits(olap)])
-	refstats$best <- subjectHits(olap)[refstats$best]
-	if (!identical(data.frame(output[!is.na(output$PValue),]), refstats)) { stop("mismatch in stats from near-identical calls!") }
+set.seed(130002)
+test_that("empiricalOverlaps works correctly", {
+    for (nreg in c(2, 10)) {
+        for (nwin in c(1, 10, 100)) {
+            regions <- generateWindows(chromos, nreg, 500)
+            windows <- generateWindows(chromos, nwin, 50)
+            
+        	olap <- findOverlaps(regions, windows)
+        	ns <- length(windows)
+        	tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
+        
+        	# Straight-up comparison to empiricalFDR, after discarding all NA's.
+        	output <- empiricalOverlaps(olap, tab)
+        	refstats <- empiricalFDR(queryHits(olap), tab[subjectHits(olap),])
+        	expect_identical(output[!is.na(output$PValue),], refstats)
+        
+        	# Testing with weights.
+        	test.weight <- runif(ns)
+        	output <- empiricalOverlaps(olap, tab, i.weight=test.weight)
+        	refstats <- empiricalFDR(queryHits(olap), tab[subjectHits(olap),], weight=test.weight[subjectHits(olap)])
+        	expect_identical(output[!is.na(output$PValue),], refstats)
+        
+        	# More weight testing, where o.weight is constructed from the weight for each i.weight.
+        	output2 <- empiricalOverlaps(olap, tab, o.weight=test.weight[subjectHits(olap)])
+        	expect_identical(output, output2)
+        }
+    }
+})
 
-	# More weight testing.
-	output2 <- getBestOverlaps(olap, tab, o.weight=test.weight[subjectHits(olap)])
-	if (!identical(output, output2)) { stop("mismatch in stats from near-identical calls!") }
+set.seed(130003)
+test_that("summitOverlaps works correctly", {
+    for (nreg in c(2, 10)) {
+        for (nwin in c(1, 10, 100)) {
+            regions <- generateWindows(chromos, nreg, 500)
+            windows <- generateWindows(chromos, nwin, 50)
 
-	return(head(output))
-}
-
-set.seed(34823)
-
-regions <- generateWindows(chromos, 10, 50)
-windows <- generateWindows(chromos, 100, 50)
-compbest(regions, windows)
-
-regions <- generateWindows(chromos, 10, 50)
-windows <- generateWindows(chromos, 200, 20)
-compbest(regions, windows)
-
-regions <- generateWindows(chromos, 2, 200)
-windows <- generateWindows(chromos, 100, 50)
-compbest(regions, windows)
-
-####################################################################################################
-
-compemp <- function(ranges, windows) {
-	olap <- findOverlaps(ranges, windows)
-	ns <- length(windows)
-	tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
-
-	# Straight-up comparison to empiricalFDR, after discarding all NA's.
-	output <- empiricalOverlaps(olap, tab)
-	refstats <- empiricalFDR(queryHits(olap), tab[subjectHits(olap),])
-	if (!identical(data.frame(output[!is.na(output$PValue),]), refstats)) { stop("mismatch in stats from near-identical calls!") }
-
-	# Testing with weights.
-	test.weight <- runif(ns)
-	output <- empiricalOverlaps(olap, tab, i.weight=test.weight)
-	refstats <- empiricalFDR(queryHits(olap), tab[subjectHits(olap),], weight=test.weight[subjectHits(olap)])
-	if (!identical(data.frame(output[!is.na(output$PValue),]), refstats)) { stop("mismatch in stats from near-identical calls!") }
-
-	# More weight testing, where o.weight is constructed from the weight for each i.weight.
-	output2 <- empiricalOverlaps(olap, tab, o.weight=test.weight[subjectHits(olap)])
-	if (!identical(output, output2)) { stop("mismatch in stats from near-identical calls!") }
-	return(head(output))
-}
-
-set.seed(34823)
-
-chromos <- c(A=1000, B=2000)
-
-regions <- generateWindows(chromos, 10, 500)
-windows <- generateWindows(chromos, 100, 50)
-compemp(regions, windows)
-
-regions <- generateWindows(chromos, 10, 500)
-windows <- generateWindows(chromos, 200, 20)
-compemp(regions, windows)
-
-regions <- generateWindows(chromos, 2, 500)
-windows <- generateWindows(chromos, 100, 50)
-compemp(regions, windows)
-
-####################################################################################################
-
-compsummit <- function(ranges, windows) { 
-	olap <- findOverlaps(ranges, windows)
-	ns <- length(windows)
-	tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
-	output <- getBestOverlaps(olap, tab)
-
-	# Checking summit calls.
-	re.weight <- summitOverlaps(olap, output$best)
-	best.win <- output$best[queryHits(olap)]
-	is.summit <- !is.na(best.win) & best.win==subjectHits(olap)
-	re.weight2a <- summitOverlaps(olap, o.summit=is.summit)
-	re.weight2b <- summitOverlaps(olap, o.summit=which(is.summit))
-	if (!identical(re.weight, re.weight2a) || !identical(re.weight, re.weight2b)) { stop("mismatch in weighting of summits") }
-
-	isummits <- rbinom(ns, 1, 0.1)==1L
-	re.weight3 <- summitOverlaps(olap, o.summit=isummits[subjectHits(olap)])
-	re.weight4 <- summitOverlaps(olap, i.summit=isummits)
-	if (!identical(re.weight3, re.weight4)) { stop("mismatch in weighting of summits") }
-
-	# Checking the core upweightSummit machinery itself.
-	by.region <- split(is.summit, queryHits(olap))	
-	nu.weight <- sapply(by.region, FUN=function(x) {
-		N <- length(x)
-		output <- rep(1, N)
-		output[x] <- N/sum(x)
-		output	
-	})
-	if (!identical(re.weight, unlist(nu.weight, use.names=FALSE))) { stop("mismatch in manual weighting confirmation") }
-
-	return(head(re.weight))
-}
-
-set.seed(34823)
-	
-regions <- generateWindows(chromos, 10, 50)
-windows <- generateWindows(chromos, 20, 50)
-compsummit(regions, windows)
-
-regions <- generateWindows(chromos, 10, 50)
-windows <- generateWindows(chromos, 20, 20)
-compsummit(regions, windows)
-
-regions <- generateWindows(chromos, 2, 200)
-windows <- generateWindows(chromos, 10, 50)
-compsummit(regions, windows)
-
-####################################################################################################
-
+        	olap <- findOverlaps(regions, windows)
+        	ns <- length(windows)
+        	tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
+        	output <- getBestOverlaps(olap, tab)
+        
+        	# Checking summit calls.
+        	re.weight <- summitOverlaps(olap, output$best)
+        	best.win <- output$best[queryHits(olap)]
+        	is.summit <- !is.na(best.win) & best.win==subjectHits(olap)
+        	re.weight2a <- summitOverlaps(olap, o.summit=is.summit)
+        	re.weight2b <- summitOverlaps(olap, o.summit=which(is.summit))
+        	expect_identical(re.weight, re.weight2a)
+            expect_identical(re.weight, re.weight2b)
+        
+        	isummits <- rbinom(ns, 1, 0.1)==1L
+        	re.weight3 <- summitOverlaps(olap, o.summit=isummits[subjectHits(olap)])
+        	re.weight4 <- summitOverlaps(olap, i.summit=isummits)
+        	expect_identical(re.weight3, re.weight4)
+        
+        	# Checking the core upweightSummit machinery itself.
+        	by.region <- split(is.summit, queryHits(olap))	
+        	nu.weight <- lapply(by.region, FUN=function(x) {
+        		N <- length(x)
+        		output <- rep(1, N)
+        		output[x] <- N/sum(x)
+        		output	
+        	})
+            if (length(nu.weight)) {
+                expect_identical(re.weight, unlist(nu.weight, use.names=FALSE))
+            } else {
+                expect_identical(re.weight, numeric(0))
+            }
+        }
+    }
+})
