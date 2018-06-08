f36610b1c7bf7ddc12378a9a824dbdee6faa14bb
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-08 09:27:17 +0100

    Added tests for remaining internal functions.

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -304,3 +304,54 @@ test_that(".extendSE works correctly", {
     expect_identical(out, ref)
 })
 
+######################################################################
+# Other miscellaneous functions.
+
+test_that("strand decisions are made correctly", {
+    expect_error(csaw:::.decideStrand(readParam(forward=NULL)), "unspecified strandedness")
+    expect_identical(csaw:::.decideStrand(readParam(forward=TRUE)), "+")
+    expect_identical(csaw:::.decideStrand(readParam(forward=FALSE)), "-")
+    expect_identical(csaw:::.decideStrand(readParam(forward=NA)), "*")
+})
+
+test_that("colData formatter works correctly", {
+    bf <- "whee.bam"
+    total <- 1011021
+    ext <- list(ext=5L)
+    all.extras <- list(list(c(1, 10), c(2, 20)))
+
+    out <- csaw:::.formatColData(bf, total, ext.data=ext, all.extras=all.extras, readParam())
+    expect_identical(out$bam.files, bf)
+    expect_identical(out$totals, total)
+    expect_identical(out$ext, ext$ext)
+    expect_equal(out$rlen, round(weighted.mean(1:2, 1:2*10)))
+
+    # Trying with multiple libraries.
+    bf <- c("whee.bam", "yay.bam")
+    total <- c(11021, 341234)
+    ext <- list(ext=c(5L, 10L))
+    all.extras <- list(list(c(1, 10), c(2, 20)), list(c(3, 30), c(4, 40)))
+
+    out <- csaw:::.formatColData(bf, total, ext.data=ext, all.extras=all.extras, readParam())
+    expect_identical(out$bam.files, bf)
+    expect_identical(out$totals, total)
+    expect_identical(out$ext, ext$ext)
+    expect_equal(out$rlen[1], round(weighted.mean(1:2, 1:2*10)))
+    expect_equal(out$rlen[2], round(weighted.mean(3:4, 3:4*10)))
+})
+
+set.seed(1005)
+test_that("automated range conversion works correctly", {
+    nrows <- 200
+	ncols <- 6
+    counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
+    rowRanges <- GRanges(rep(c("chr1", "chr2"), c(50, 150)),
+                         IRanges(floor(runif(200, 1e5, 1e6)), width=100),
+                         strand=sample(c("+", "-"), 200, TRUE))
+    rse <- SummarizedExperiment(list(counts=counts), rowRanges=rowRanges)
+    
+    expect_identical(rowRanges(rse), csaw:::.toGRanges(rse))
+    expect_identical(rowRanges(rse), csaw:::.toGRanges(rowRanges(rse)))
+    expect_error(csaw:::.toGRanges("whee"), "must be a RangedSummarizedExperiment")
+})
+
