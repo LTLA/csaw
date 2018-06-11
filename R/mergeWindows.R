02522b53e3896013293448efd9c86355a0a11356
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-11 19:38:44 +0100

    Simplified merging of nested windows of opposing sign. Switched from tab to 4-space indenting. Roxygenated mergeWindows.

--- R/mergeWindows.R
+++ R/mergeWindows.R
@@ -1,3 +1,10 @@
+#' @export
+#' @importFrom BiocGenerics strand strand<-
+#' @importFrom S4Vectors runValue
+#' @importFrom GenomeInfoDb seqnames Seqinfo seqlevels seqlengths
+#' @importFrom GenomicRanges GRanges
+#' @importFrom IRanges IRanges
+#' @importFrom stats start end
 mergeWindows <- function(regions, tol, sign=NULL, max.width=NULL, ignore.strand=TRUE)
 # This function merges the adjacent windows if they lie within 'tol' of each other,
 # Any abundance filtering should be done beforehand. Negative values of tol refer
@@ -6,57 +13,64 @@ mergeWindows <- function(regions, tol, sign=NULL, max.width=NULL, ignore.strand=
 # 
 # written by Aaron Lun
 # created 30 July 2013
-# last modified 8 January 2016
 {
     regions <- .toGRanges(regions)
-	strs <- strand(regions)
-	if (!ignore.strand && length(runValue(strs))!=1) {
-		# Strand-specific clustering.
-		forward <- as.logical(strs=="+")
-		reverse <- as.logical(strs=="-")
-		neither <- as.logical(strs=="*")
-		ids <- integer(length(regions))
-		regs <- NULL
-		if (any(forward)) { 
-			out <- Recall(regions=regions[forward], tol=tol, sign=sign[forward], max.width=max.width, ignore.strand=TRUE) 
-			ids[forward] <- out$id
-			strand(out$region) <- "+"
-			regs <- out$region
-		}
-		if (any(reverse)) { 
-			out <- Recall(regions=regions[reverse], tol=tol, sign=sign[reverse], max.width=max.width, ignore.strand=TRUE) 
-			ids[reverse] <- out$id + length(regs)
-			strand(out$region) <- "-"
-			regs <- c(regs, out$region)
-		}
-		if (any(neither)) { 
-			out <- Recall(regions=regions[neither], tol=tol, sign=sign[neither], max.width=max.width, ignore.strand=TRUE) 
-			ids[neither] <- out$id + length(regs)
-			regs <- c(regs, out$region)
-		}
-		return(list(id=ids, region=regs))
-	}
+    strs <- strand(regions)
+    if (!ignore.strand && length(runValue(strs))!=1) {
+        # Strand-specific clustering.
+        forward <- as.logical(strs=="+")
+        reverse <- as.logical(strs=="-")
+        neither <- as.logical(strs=="*")
 
-	# Setting up necessary parameters.
-	chrs <- as.integer(seqnames(regions))
-	starts <- start(regions)
-	ends <- end(regions)
-	o <- order(chrs, starts, ends)
-	if (is.null(sign)) { 
-		sign <- logical(length(regions)) 
-	} else {
+        ids <- integer(length(regions))
+        regs <- vector("list", 3)
+        collected <- 0L
+
+        if (any(forward)) { 
+            out <- Recall(regions=regions[forward], tol=tol, sign=sign[forward], max.width=max.width, ignore.strand=TRUE) 
+            ids[forward] <- out$id
+            strand(out$region) <- "+"
+            regs[[1]] <- out$region
+            collected <- collected + length(out$region)
+        }
+
+        if (any(reverse)) { 
+            out <- Recall(regions=regions[reverse], tol=tol, sign=sign[reverse], max.width=max.width, ignore.strand=TRUE) 
+            ids[reverse] <- out$id + collected
+            strand(out$region) <- "-"
+            regs[[2]] <- out$region
+            collected <- collected + length(out$region)
+        }
+
+        if (any(neither)) { 
+            out <- Recall(regions=regions[neither], tol=tol, sign=sign[neither], max.width=max.width, ignore.strand=TRUE) 
+            ids[neither] <- out$id + collected
+            regs[[3]] <- out$region
+        }
+        return(list(id=ids, region=do.call(c, regs)))
+    }
+
+    tol <- as.integer(tol)
+    max.width <- as.integer(max.width)
+
+    chrs <- as.integer(seqnames(regions))
+    starts <- start(regions)
+    ends <- end(regions)
+    o <- order(chrs, starts, ends)
+
+    if (is.null(sign)) { 
+        sign <- logical(length(regions)) 
+    } else {
         if (length(sign)!=length(regions)) {
             stop("lengths of 'sign' and 'ids' must be the same")
         }
-		sign <- sign[o]
-	}
-	tol <- as.integer(tol)
-	max.width <- as.integer(max.width)
+        sign <- sign[o]
+    }
 
-	# Running the merge. Indices correspond with positions in 'clustered'.
-	out <- .Call(cxx_merge_windows, chrs[o], starts[o], ends[o], sign, tol, max.width)
-	out[[1]][o] <- out[[1]]
-	clustered <- GRanges(levels(seqnames(regions))[out[[2]]], IRanges(out[[3]], out[[4]]),
-			seqinfo=Seqinfo(seqlevels(regions), seqlengths(regions)))
-	return(list(id=out[[1]], region=clustered))
+    # Running the merge. Indices correspond with positions in 'clustered'.
+    out <- .Call(cxx_merge_windows, chrs[o], starts[o], ends[o], sign, tol, max.width)
+    out[[1]][o] <- out[[1]]
+    clustered <- GRanges(levels(seqnames(regions))[out[[2]]], IRanges(out[[3]], out[[4]]),
+            seqinfo=Seqinfo(seqlevels(regions), seqlengths(regions)))
+    return(list(id=out[[1]], region=clustered))
 }
--- src/merge_windows.cpp
+++ src/merge_windows.cpp
@@ -1,160 +1,128 @@
 #include "csaw.h"
 #include "utils.h"
 
-int split_cluster(const Rcpp::IntegerVector&, const Rcpp::IntegerVector&, Rcpp::IntegerVector&, const int&, const int&, const int&, const int&);
+int split_cluster(const Rcpp::IntegerVector& starts, const Rcpp::IntegerVector& ends, Rcpp::IntegerVector& output, 
+    const int& first, const int& last, const int& width);
 
 /* We assume that incoming elements are sorted by chr -> start -> end. We then proceed 
  * to aggregate elements by chaining together elements that are less than 'tolerance'
- * apart and, if required, have the same 'sign'. We also split them if the difference
- * between the first start and the last end is greater than 'max_size'.
+ * apart and, if required, have the same 'sign'. 
  */
 
-SEXP merge_windows(SEXP chrs, SEXP start, SEXP end, SEXP sign, SEXP tolerance, SEXP max_size) {
+SEXP merge_windows(SEXP win_chrs, SEXP win_start, SEXP win_end, SEXP win_sign, SEXP tolerance, SEXP max_size) {
     BEGIN_RCPP
 
-    const Rcpp::IntegerVector _chrs(chrs), _start(start), _end(end);
-    const Rcpp::LogicalVector _sign(sign);
-	const int n = _chrs.size();
-	if (n!=_start.size() || n!=_end.size() || n!=_sign.size()) { 
+    const Rcpp::IntegerVector chrs(win_chrs), start(win_start), end(win_end);
+    const Rcpp::LogicalVector sign(win_sign);
+    const int nwin = chrs.size();
+    if (nwin!=start.size() || nwin!=end.size() || nwin!=sign.size()) { 
         throw std::runtime_error("lengths of vectors are not equal"); 
     }
 
     const int tol=check_integer_scalar(tolerance, "tolerance");
-    const Rcpp::IntegerVector _max_size(max_size);
-	if (_max_size.size() > 1) { 
+    const Rcpp::IntegerVector maxregion(max_size);
+    if (maxregion.size() > 1) { 
         throw std::runtime_error("maximum size should be an integer scalar or NULL"); 
     }
-	const bool limit_size=(_max_size.size()==1);
-	const int maxs=(limit_size ? _max_size[0] : 0);
-	
-	// Proceeding with the merge operation.
-    Rcpp::IntegerVector out_index(n);
-	int start_index=0;
+    const bool limit_size=(maxregion.size()==1);
+    const int maxs=(limit_size ? maxregion[0] : 0);
+
+    // Proceeding with the merge operation.
+    Rcpp::IntegerVector out_index(nwin);
+    int first_of_run=0;
     int ngroups=0;
-    int last_end, last_sign;
-    if (n) { 
+    if (nwin) { 
         ngroups=1;
-        last_end=_end[0];
-        last_sign=_sign[0];
         out_index[0]=1;
     }
-    bool warned=false;
 
-	for (int i=1; i<n; ++i) {
-        bool diffchr=(_chrs[i]!=_chrs[i-1]);
-        bool diffsign=(_sign[i]!=last_sign);
-        if (!diffchr && _start[i] < _start[i-1]) { 
-            throw std::runtime_error("regions should be sorted by start position");
-        }
-
-		/* Fully nested regions don't have a properly defined interpretation when it comes
-		 * to splitting things by sign. We only support consideration of nested regions where
-		 * either of the boundaries are the same. That can be considered to break the 
-		 * previous stretch if it had opposite sign. Otherwise, the next window would have to
-		 * make the decision to match the parent window or its nested child.
-		 *
-		 * If the start is the same, the window with the earlier end point should be ordered
-		 * first, so that won't pass the first 'if'. This means that it'll only enter with
-		 * a fully nested window. Start and end-point equality might be possible at the ends 
-		 * of chromosomes where trimming enforces sameness, but full nesting should not be observed.
-		 *
-		 * If the nested region has the same sign as the parent, then everything proceeds
-		 * normally i.e. same cluster. We make sure to keep 'last_end' as the parent end in
-		 * such cases. This ensures that the following windows get a change to compute
-		 * distances to the parent end (which should be closer).
-		 */
-        bool nested=(!diffchr && _end[i] < last_end);
-	    if (nested) { 
-		   	if (diffsign && !warned) { 
-                Rcpp::warning("fully nested windows of opposite sign are present and ignored"); 
-                warned=true;
-                diffsign=false;
+    for (int i=1; i<nwin; ++i) {
+        if (chrs[i]!=chrs[i-1]                                  // Obviously, changing if we're on a different chromosome.
+            || start[i] - end[i-1] - 1 > tol                    // Space between windows, start anew if this is greater than the tolerance.
+            || sign[i]!=sign[i-1]                               // Checking if the sign is consistent.
+           ) {
+            if (limit_size) {
+                ngroups = split_cluster(start, end, out_index, first_of_run, i, maxs);
             }
-		} 
-
-		if (diffchr 											// Obviously, changing if we're on a different chromosome.
-			|| _start[i]-last_end-1 > tol						// Space between windows, start anew if this is greater than the tolerance.
-			|| diffsign 										// Checking if the sign is consistent.
-	   	) {
-		    if (limit_size) { 
-                // Splitting the cluster, if desired.
-                ngroups=split_cluster(_start, _end, out_index, last_end, start_index, i, maxs); 
-            } 
-			++ngroups;
-			out_index[i]=ngroups; 
-			start_index=i;
-		} else {
-			out_index[i]=out_index[i-1];
-		}
-
-        // Using the parent window as the endpoint if it's nested, but otherwise bumping up the last stats.
-        if (!nested) { 
-            last_end=_end[i]; 
-            last_sign=_sign[i];
+            ++ngroups;
+            first_of_run=i;
         }
-	}
 
-	// Cleaning up the last cluster, if necessary.
-  	if (limit_size) { 
-        ngroups=split_cluster(_start, _end, out_index, last_end, start_index, n, maxs); 
+        out_index[i]=ngroups; 
     }
 
-	// Now, identifying the chromosome, start and end of each region.
-    Rcpp::IntegerVector out_chr(ngroups, -1), out_start(ngroups, -1), out_end(ngroups, -1);
+    // Cleaning up the last cluster, if necessary.
+    if (limit_size) {
+        ngroups = split_cluster(start, end, out_index, first_of_run, nwin, maxs);
+    }
 
-	for (int i=0; i<n; ++i) { 
-		int curgroup=out_index[i]-1;
-		if (out_chr[curgroup]<0) { 
-			out_chr[curgroup]=_chrs[i];
-			out_start[curgroup]=_start[i]; // Sorted by start, remember; only need this once.
-			out_end[curgroup]=_end[i];
-		} else if (out_end[curgroup] < _end[i]) { 
-		    out_end[curgroup]=_end[i]; 
-		}
-	}
-	
+    // Now, identifying the chromosome, start and end of each region.
+    Rcpp::IntegerVector out_chr(ngroups, -1), out_start(ngroups), out_end(ngroups);
+
+    for (int i=0; i<nwin; ++i) { 
+        int curgroup=out_index[i]-1;
+        if (out_chr[curgroup]<0) { 
+            out_chr[curgroup]=chrs[i];
+            out_start[curgroup]=start[i]; // Sorted by start, remember; only need this once.
+            out_end[curgroup]=end[i];
+        } else if (out_end[curgroup] < end[i]) { 
+            out_end[curgroup]=end[i]; 
+        }
+    }
+
     return Rcpp::List::create(out_index, out_chr, out_start, out_end);
     END_RCPP
 }
 
 int split_cluster(const Rcpp::IntegerVector& starts, const Rcpp::IntegerVector& ends, Rcpp::IntegerVector& output, 
-        const int& actual_end, const int& xs, const int& xe, const int& width) {
+        const int& first, const int& last, const int& width) {
+    
+    if (last - first == 1) {
+        return output[first];
+    }
+    const int true_end=*std::max_element(ends.begin() + first, ends.begin() + last);
+    const int full_width=true_end - starts[first] + 1;
+    if (full_width <= width) { 
+        return output[first];
+    }
 
-	double full_width=actual_end-starts[xs]+1;
-	if (full_width <= width) { return output[xs]; }
-	int mult=int(std::ceil(full_width/width));
-
-	/* There can only be `mult` subclusters. At the worst, `cur_diff`
-	   will be equal to `actual_end - starts[xs]`. Division by `subwidth` will
-	   give an expression of `(actual_end - starts[xs]) / [ (actual_end - starts[xs] + 1) / mult ]`.
-	   This will always be less than `mult`, so flooring it will give `mult-1`,
-	   i.e., the last index of `instantiated`.
-	 */
+    /* We split up the cluster into evenly sized partitions where each partition is less than 'width'.
+     * There are 'mult' partitions in total, each of width 'subwidth'.
+     * Windows are assigned to partitions based on their midpoints. 
+     * 
+     * Note that there can only be `mult` subclusters. 
+     * In the most extreme case, `cur_diff` will be equal to `true_end - starts[first]`. 
+     * Division by `subwidth` will give an expression of `(true_end - starts[first]) / [ (true_end - starts[first] + 1) / mult ]`.
+     * This will always be less than `mult`, so flooring it will give `mult-1`, i.e., the last index of `instantiated`.
+     */
+       const int mult=int(std::ceil(double(full_width)/width));
     std::vector<int> instantiated(mult, 0);
-	int output_index=output[xs];
+    int cur_group=output[first];
 
-	// Allocating windows into subclusters, based on their midpoints.
-	double subwidth=full_width/mult, cur_diff;
-	for (int i=xs; i<xe; ++i) {
-		cur_diff = double(starts[i]+ends[i])*0.5 - starts[xs];
-		output[i] = int(cur_diff/subwidth);	
-		if (!instantiated[output[i]]) { instantiated[output[i]] = 1; }
-	}
+    const double subwidth=double(full_width)/mult;
+    for (int i=first; i<last; ++i) {
+        const double cur_diff = double(starts[i]+ends[i])*0.5 - starts[first];
+        output[i] = int(cur_diff/subwidth);    
+        if (!instantiated[output[i]]) { 
+            instantiated[output[i]] = 1; 
+        }
+    }
 
-	/* Allocating output indices to the subclusters. This approach avoids
-	   situations where you get nonconsecutive cluster indices, e.g., when
-	   `tol` is greater than the maximum width.	 
-	 */
-	for (int i=0; i<mult; ++i) { 
-		if (!instantiated[i]) { continue; }
-		instantiated[i]=output_index;
-		++output_index;
-	}
+    // Allocating output indices to the subclusters. This avoids nonconsecutive cluster indices.
+    for (int i=0; i<mult; ++i) { 
+        if (!instantiated[i]) { 
+            continue; 
+        }
+        instantiated[i]=cur_group;
+        ++cur_group;
+    }
 
-	// Assigning indices back to the output vector.
-	for (int i=xs; i<xe; ++i) { output[i]=instantiated[output[i]]; }
+    // Assigning indices back to the output vector.
+    for (int i=first; i<last; ++i) { 
+        output[i]=instantiated[output[i]]; 
+    }
 
-	// Returning the last group index that was used.
-	return output_index-1;	
+    // Returning the new number of groups.
+    return cur_group - 1;
 }
 
