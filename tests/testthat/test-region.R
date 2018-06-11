bbe0f2e8d5a9aafe4db08e4337bb9dc138f04f5c
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-11 09:38:11 +0100

    Toughened up restrict= tests in regionCounts, checked output ranges.

--- tests/testthat/test-region.R
+++ tests/testthat/test-region.R
@@ -5,7 +5,15 @@ source("simsam.R")
 
 library(Rsamtools)
 CHECKFUN <- function(bam.files, regions, param, fraglen=200, final.len=NA) {
-	y <- regionCounts(bam.files, regions=regions, ext=fraglen, param=param)
+    y <- regionCounts(bam.files, regions=regions, ext=fraglen, param=param)
+    if (is.na(param$forward)) { 
+        expect_identical(rowRanges(y), regions)
+    } else {
+        tmp <- regions
+        strand(tmp) <- ifelse(param$forward, "+", "-")
+        expect_identical(rowRanges(y), tmp)
+    }
+
     all.chrs <- scanBamHeader(bam.files[1])[[1]]$targets
     genome <- GRanges(names(all.chrs), IRanges(1, all.chrs))
 
@@ -21,8 +29,8 @@ CHECKFUN <- function(bam.files, regions, param, fraglen=200, final.len=NA) {
                 expect_identical(assay(y)[chosen,f], countOverlaps(regions[chosen], collected))
                 total.reads[f] <- total.reads[f] + length(collected)
             }
-		}
-	}
+        }
+    }
 
     expect_identical(total.reads, y$totals)
     return(y)
@@ -39,10 +47,9 @@ test_that("regionCounts works with SE data", {
                         readParam(dedup=TRUE),
                         readParam(forward=FALSE),
                         readParam(forward=TRUE),
-                        readParam(discard=makeDiscard(10, 100, chromos)),
-                        readParam(restrict="chrB")
+                        readParam(discard=makeDiscard(10, 100, chromos))
                         )) {
-    	bam.files <-c(regenSE(1000, chromos, file.path(tempdir, "A")), 
+        bam.files <-c(regenSE(1000, chromos, file.path(tempdir, "A")), 
                       regenSE(2000, chromos, file.path(tempdir, "B")))
         extractor <- generateWindows(chromos, 5, 500)
         CHECKFUN(bam.files, extractor, param=rparam)
@@ -50,20 +57,32 @@ test_that("regionCounts works with SE data", {
 
     # Checking extension strategies.
     for (ext.param in list(NA, 10, 100, 100)) {
-    	bam.files <-c(regenSE(1000, chromos, file.path(tempdir, "A")), 
+        bam.files <-c(regenSE(1000, chromos, file.path(tempdir, "A")), 
                       regenSE(1500, chromos, file.path(tempdir, "B")))
         extractor <- generateWindows(chromos, 5, 500)
         CHECKFUN(bam.files, extractor, param=rparam, fraglen=ext.param)
-   }
+    }
 
     # Checking restriction strategies.
     bam.files <-c(regenSE(600, chromos, file.path(tempdir, "A")), 
                   regenSE(500, chromos, file.path(tempdir, "B")))
+
     extractor <- generateWindows(chromos, 5, 500)
-    y1 <- CHECKFUN(bam.files, extractor, param=readParam())
-    y2 <- CHECKFUN(bam.files, extractor, param=readParam(restrict=names(chromos)))
+    y1 <- CHECKFUN(bam.files, extractor, param=readParam(), fraglen=200)
+    y2 <- regionCounts(bam.files, extractor, param=readParam(restrict=names(chromos)), ext=200)
     expect_identical(assay(y1), assay(y2))
     expect_identical(y1$totals, y2$totals)
+
+    yA <- regionCounts(bam.files, extractor, param=readParam(restrict="chrA"), ext=200)
+    expect_true(all(assay(yA)[as.logical(seqnames(extractor)!="chrA"),]==0))
+    expect_identical(rowRanges(yA), extractor)
+
+    yB <- regionCounts(bam.files, extractor, param=readParam(restrict="chrB"), ext=200)
+    expect_true(all(assay(yB)[as.logical(seqnames(extractor)!="chrB"),]==0))
+    expect_identical(rowRanges(yA), extractor)
+
+    expect_equal(assay(yA) + assay(yB), assay(y1))
+    expect_equal(yA$totals + yB$totals, y1$totals)
 })
 
 set.seed(40002)
@@ -71,7 +90,7 @@ test_that("regionCounts works with PE data", {
     for (rparam in list(readParam(pe="both"), 
                         readParam(pe="both", max.frag=1e8),
                         readParam(pe="both", max.frag=200))) {
-    	bam.files <-c(regenPE(10000, chromos, file.path(tempdir, "A")), 
+        bam.files <-c(regenPE(10000, chromos, file.path(tempdir, "A")), 
                       regenPE(11000, chromos, file.path(tempdir, "B")))
         extractor <- generateWindows(chromos, 5, 500)
         CHECKFUN(bam.files, extractor, param=rparam)
@@ -80,12 +99,12 @@ test_that("regionCounts works with PE data", {
     # Checking alternative counting from PE data.
     for (rparam in list(readParam(pe="first"), 
                         readParam(pe="second"))) {
-    	bam.files <-c(regenPE(20000, chromos, file.path(tempdir, "A")), 
+        bam.files <-c(regenPE(20000, chromos, file.path(tempdir, "A")), 
                       regenPE(10000, chromos, file.path(tempdir, "B")))
         extractor <- generateWindows(chromos, 5, 500)
         CHECKFUN(bam.files, extractor, param=rparam, fraglen=100)
         CHECKFUN(bam.files, extractor, param=rparam, fraglen=NA)
-   }
+    }
 })
 
 set.seed(40002)
