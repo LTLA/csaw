194a9be06cd78a8ebed2c73f0cc9b377360f1715
Aaron Lun <alun@wehi.edu.au>
2018-06-19 15:59:39 +0100

    Split getWidths into its own source file.

---
+++ R/getWidths.R
@@ -0,0 +1,29 @@
+#' @export
+#' @importFrom BiocGenerics width
+#' @importFrom S4Vectors metadata
+#' @importFrom SummarizedExperiment rowRanges
+getWidths <- function(data) 
+# This computes the effective width of the data in the
+# RangedSummarizedExperiment object. This is done by accounting for the effect
+# of read extension; or, for paired end data, the median fragment length.
+#
+# written by Aaron Lun
+# created 5 November 2014
+{
+	flen <- metadata(data)$final.ext
+
+	if (is.na(flen)) { 
+		flen <- data$ext
+		is.missing <- is.na(flen)
+		if (any(is.missing)) { 
+			if (is.null(data$rlen) || any(is.na(data$rlen[is.missing]))) { 
+				stop("need to specify read lengths in 'data$rlen'")
+			}
+			flen[is.missing] <- data$rlen[is.missing]
+		}
+		flen <- as.integer(mean(flen))
+	}
+
+	width(rowRanges(data)) + flen - 1L
+}
+
