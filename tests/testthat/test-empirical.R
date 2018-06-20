3f7e8184d0015721392e97a05c2f23fe8918a1f6
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-20 14:00:00 +0100

    Hardened comparison observed FDR for empiricalFDR().

--- tests/testthat/test-empirical.R
+++ tests/testthat/test-empirical.R
@@ -51,18 +51,37 @@ test_that("empiricalFDR works correctly with vanilla input", {
 
 set.seed(90001)
 test_that("empiricalFDR controls the FDR correctly", {
-    for (ntrue in c(1000, 2000)) {
-        for (nfalse in c(2000, 5000)) {
-            overall <- numeric(20)
-            for (it in seq_along(overall)) {
-                clust.out <- autogen(1000, 5000, 10000)
-                out <- empiricalFDR(clust.out$id, clust.out$tab)
-    
-                is.sig <- out$FDR <= 0.05
+    for (ntrue in c(500, 1000, 2000, 5000)) {
+        for (nfalse in c(500, 1000, 2000, 5000)) {
+            original <- autogen(ntrue, nfalse, 10000)
+            clust.out <- original
+
+            # Forcing the p-value distribution for true nulls to be the same for pos/neg logFCs.
+            # This ensures that the observed FDR == empirical FDR.
+            fakes <- clust.out$id <= nfalse
+            clust.out$id <- c(clust.out$id[fakes], clust.out$id[fakes], clust.out$id[!fakes])
+
+            retab1 <- retab2 <- clust.out$tab[fakes,]
+            retab1$logFC <- abs(retab1$logFC)
+            retab2$logFC <- -abs(retab2$logFC)
+            clust.out$tab <- rbind(retab1, retab2, clust.out$tab[!fakes,])
+
+            # Empirical FDR MUST be below the threshold.
+            out <- empiricalFDR(clust.out$id, clust.out$tab)
+            for (target in c(0.01, 0.05, 0.1)) { 
+                is.sig <- out$FDR <= target
                 is.false <- as.integer(rownames(out)) <= nfalse 
-                overall[it] <- sum(is.sig & is.false)/sum(is.sig)
+                expect_true(!any(is.sig) || sum(is.sig & is.false)/sum(is.sig) <= target)
             }
-            expect_true(mean(overall) < 0.05  * 1.1) # tolerance.
+
+            # Same as above, but with new IDs for the true null with pos/neg log-fold changes.
+            clust.out$id <- c(original$id[fakes], original$id[fakes] + nfalse, original$id[!fakes] + nfalse)
+            out <- empiricalFDR(clust.out$id, clust.out$tab)
+            for (target in c(0.01, 0.05, 0.1)) { 
+                is.sig <- out$FDR <= target
+                is.false <- as.integer(rownames(out)) <= nfalse * 2L
+                expect_true(!any(is.sig) || sum(is.sig & is.false)/sum(is.sig) <= target)
+            }
         }
     }
 })
