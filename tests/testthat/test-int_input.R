501a087cf0c6737877b5e7f9d55d8a5f99ec9d88
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-22 17:24:05 +0100

    Added tests for test-input checking functions.

--- tests/testthat/test-int_input.R
+++ tests/testthat/test-int_input.R
@@ -1,4 +1,4 @@
-# This tests the basic internal functions on which all others are built.
+# This tests the basic internal input functions.
 # library(testthat); library(csaw); source("test-int_input.R")
 
 source("simsam.R")
@@ -40,3 +40,82 @@ test_that("automated range conversion works correctly", {
     expect_error(csaw:::.toGRanges("whee"), "must be a RangedSummarizedExperiment")
 })
 
+set.seed(320002)
+test_that("test input checker is working correctly", {
+    nobs <- 200L
+    ids <- sample(100, nobs, replace=TRUE)
+    tab <- data.frame(whee=runif(nobs), yay=seq_len(nobs))
+
+    # Simple integer ids.
+    incoming <- csaw:::.check_test_inputs(ids, tab, NULL)
+    expect_identical(incoming$ids, sort(as.integer(factor(ids))))
+    expect_identical(incoming$tab, tab[order(ids),])
+    expect_identical(incoming$original, order(ids))
+    expect_identical(incoming$weight, rep(1, nobs))
+    expect_identical(incoming$groups, as.character(sort(unique(ids))))
+
+    # Weights.
+    w <- runif(nobs)
+    incoming <- csaw:::.check_test_inputs(ids, tab, w)
+    expect_identical(incoming$weight, w[order(ids)])
+
+    # More complex IDs.
+    ids2 <- sample(LETTERS, nobs, replace=TRUE)
+    incoming <- csaw:::.check_test_inputs(ids2, tab, NULL)
+    expect_identical(incoming$ids, sort(as.integer(factor(ids2))))
+    expect_identical(incoming$tab, tab[order(ids2),])
+    expect_identical(incoming$original, order(ids2))
+    expect_identical(incoming$weight, rep(1, nobs))
+    expect_identical(incoming$groups, sort(unique(ids2)))
+
+    # Handles NAs.
+    ids0 <- ids
+    ids0[sample(nobs, 20)] <- NA
+    out <- csaw:::.check_test_inputs(ids0, tab, NULL)
+    keep <- !is.na(ids0)
+    ref <- csaw:::.check_test_inputs(ids0[keep], tab[keep,], NULL)
+    ref$original <- which(keep)[ref$original]
+    expect_identical(out, ref)
+
+    w <- runif(nobs)
+    out <- csaw:::.check_test_inputs(ids0, tab, w)
+    keep <- !is.na(ids0)
+    ref <- csaw:::.check_test_inputs(ids0[keep], tab[keep,], w[keep])
+    ref$original <- which(keep)[ref$original]
+    expect_identical(out, ref)
+
+    # Behaves on empty inputs.
+    out <- csaw:::.check_test_inputs(ids[0], tab[0,], NULL)
+    expect_identical(out$ids, integer(0))
+    expect_identical(out$groups, character(0))
+    expect_identical(out$tab, tab[0,])
+    expect_identical(out$weight, numeric(0))
+    expect_identical(out$original, integer(0))
+
+    # Checking errors.
+    expect_error(csaw:::.check_test_inputs(ids, tab[0,], NULL), "is not TRUE")
+    expect_error(csaw:::.check_test_inputs(ids, tab, runif(10)), "is not TRUE")
+})
+
+set.seed(320003)
+test_that("column parsers for test results are working correctly", {
+    tab <- data.frame(logFC=rnorm(1000), PValue=runif(1000))
+    expect_identical(csaw:::.parseFCcol(NULL, tab), 1L)
+    expect_identical(csaw:::.parseFCcol("logFC", tab), 1L)
+    expect_identical(csaw:::.parseFCcol(1, tab), 1L)
+
+    # Handles multiple inputs.
+    tab2 <- data.frame(logFC.A=rnorm(1000), logFC.B=runif(1000))
+    expect_identical(csaw:::.parseFCcol(NULL, tab2), 1:2)
+    expect_identical(csaw:::.parseFCcol(2, tab2), 2L)
+    expect_identical(csaw:::.parseFCcol(c("logFC.B", "logFC.A"), tab2), 2:1)
+
+    expect_error(csaw:::.parseFCcol(NULL, tab2, multiple=FALSE), "exactly one column")
+    expect_error(csaw:::.parseFCcol("YAY", tab2), "failed to match")
+
+    # Parses p-value columns.
+    expect_identical(csaw:::.getPValCol(NULL, tab), 2L)
+    expect_identical(csaw:::.getPValCol("PValue", tab), 2L)
+    expect_identical(csaw:::.getPValCol(2, tab), 2L)
+    expect_error(csaw:::.getPValCol("urgh", tab), "failed to find")
+})
