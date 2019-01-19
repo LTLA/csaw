d9366ea355976cad8824ae545dbbbadf92d7886f
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-01-19 15:11:39 +0000

    Avoid Depends: on BiocParallel, remove R version dependency. Bumped version number and date.

--- R/namespace.R
+++ R/namespace.R
@@ -1,6 +1,5 @@
 #' @importFrom Rcpp sourceCpp
 #' @import SummarizedExperiment
-#' @import BiocParallel
 #' @import GenomicRanges
 #' @import methods
 #' @useDynLib csaw, .registration=TRUE, .fixes="cxx_"
