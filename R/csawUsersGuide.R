d42d56a728115b203135417f3e5d67313cd0a1fc
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-17 17:53:11 +0000

    Updated csawUsersGuide to use browseURL to workflow package.

--- R/csawUsersGuide.R
+++ R/csawUsersGuide.R
@@ -1,17 +1,9 @@
 #' @export
-csawUsersGuide <- function(view=TRUE)
-# Find, and optionally show, the csaw user's guide 
+#' @importFrom utils browseURL
+csawUsersGuide <- function()
+# Show the csaw user's guide 
 # 
-# written by Aaron Lun, based on equivalent from edgeR by Gordon Smyth
-# 21 June 2014.
+# written by Aaron Lun.
 {
-	ugloc <- system.file("doc", "csawUserGuide.pdf", package="csaw")
-	if (view) {
-		if(.Platform$OS.type == "windows") {
-			shell.exec(ugloc)
-		} else {
-			system(paste(Sys.getenv("R_PDFVIEWER"),ugloc,"&"))
-		}
-	}
-	return(ugloc)
+    browseURL(sprintf("http://bioconductor.org/%s/workflows/vignettes/csawUsersGuide/inst/doc/csawUsersGuide.pdf", BiocManager::version()))
 }
