b903f73d357c07f19d47523583f7db1069dcb2d9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-05-28 22:50:18 -0700

    Deprecated functionality in consolidateWindows.

--- R/consolidateWindows.R
+++ R/consolidateWindows.R
@@ -11,30 +11,40 @@ consolidateWindows <- function(data.list, equiweight=TRUE, merge.args=list(), re
 {
 	if (is.null(region)) { 
         .Deprecated(new="consolidateByMerge")
-        output <- do.call(consolidateByMerge, c(list(data.list, equiweight=equiweight, sign.list=sign.list), merge.args))
+        output <- do.call(consolidateByMerge, c(list(data.list, sign.list=sign.list), merge.args))
+
+        weight <- if (equiweight) {
+            split(output$ranges$weight, output$ranges$origin)
+        } else {
+            NULL
+        }
+
         list(
-            id=split(output$original$id, output$original$origin), 
-            weight=split(output$original$weight, output$original$origin),
-            region=output$region
+            id=split(output$ranges$id, output$ranges$origin), 
+            weight=weight, 
+            region=output$merged
         )
 
 	} else {
         .Deprecated(new="consolidateByOverlap")
         output <- do.call(consolidateByOverlap, c(list(data.list, equiweight=equiweight, region=region), overlap.args))
 
-        original.index <- unlist(lapply(data.list, seq_along))
-        subject.origin <- output$original$origin[subjectHits(output$olap)]
+        ranges.index <- unlist(lapply(data.list, seq_along))
+        subject.origin <- output$ranges$origin[subjectHits(output$olap)]
 
-        by.original <- split(output$olap, subject.origin)
-        for (i in seq_along(by.original)) {
-            current <- by.original[[i]]
-            by.original[[i]] <- Hits(queryHits(current), original.index[subjectHits(current)],
+        by.ranges <- split(output$olap, subject.origin)
+        for (i in seq_along(by.ranges)) {
+            current <- by.ranges[[i]]
+            by.ranges[[i]] <- Hits(queryHits(current), ranges.index[subjectHits(current)],
                 nLnode=nLnode(current), nRnode=nRnode(current), sort.by.query=TRUE)
         }
 
-        list(
-            olap=by.original,
-            weight=split(output$weight, subject.origin)
-        )
+        weight <- if (equiweight) {
+            split(output$olap$weight, subject.origin)
+        } else {
+            NULL
+        }
+
+        list(olap=by.ranges, weight=weight)
 	}
 }
