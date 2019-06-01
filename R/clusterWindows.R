e73e3c851feb0ba5be83940f7b16c2027f19756c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-01 15:16:19 -0700

    Renamed inputs, outputs of clusterWindows for consistency.

--- R/clusterWindows.R
+++ R/clusterWindows.R
@@ -1,13 +1,15 @@
 #' @export
-clusterWindows <- function(regions, tab, target, pval.col=NULL, fc.col=NA, tol, ..., weights=NULL, grid.length=21, iterations=4)
+clusterWindows <- function(ranges, tab, target, pval.col=NULL, fc.col=NA, tol, ..., weights=NULL, grid.length=21, iterations=4)
 # This does a search for the clusters based on DB windows. 
 # It aims to achieve a cluster-level FDR of 'target'.
 #
 # written by Aaron Lun
 # created 8 January 2016
 {
-    regions <- .toGRanges(regions)
-    if (nrow(tab)!=length(regions)) { stop("number of regions is not consistent with entries in 'tab'") }
+    ranges <- .toGRanges(ranges)
+    if (nrow(tab)!=length(ranges)) { 
+        stop("number of ranges is not consistent with entries in 'tab'") 
+    }
     if (missing(tol)) {
         tol <- 100
         warning("'tol' for 'mergeWindows' set to a default of 100 bp")
@@ -19,7 +21,9 @@ clusterWindows <- function(regions, tab, target, pval.col=NULL, fc.col=NA, tol, 
 
     # Computing a frequency-weighted adjusted p-value.
     pval.col <- .getPValCol(pval.col, tab)
-    if (is.null(weights)) { weights <- rep(1, nrow(tab)) }
+    if (is.null(weights)) { 
+        weights <- rep(1, nrow(tab)) 
+    }
     adjp <- .weightedFDR(tab[,pval.col], weights)
 
     # Getting the sign.
@@ -30,18 +34,21 @@ clusterWindows <- function(regions, tab, target, pval.col=NULL, fc.col=NA, tol, 
     }
 
     # Controlling the cluster-level FDR
-    FUN <- function(sig) { mergeWindows(regions[sig], tol=tol, signs=sign[sig], ...) }
-    out <- controlClusterFDR(target=target, adjp=adjp, FUN=function(sig) { FUN(sig)$id }, 
-                             weights=weights, grid.length=grid.length, iterations=iterations)
+    FUN <- function(sig) { 
+        mergeWindows(ranges[sig], tol=tol, signs=sign[sig], ...) 
+    }
+    out <- controlClusterFDR(target=target, adjp=adjp, 
+        FUN=function(sig) { FUN(sig)$ids }, 
+        weights=weights, grid.length=grid.length, iterations=iterations)
     sig <- adjp <= out$threshold
     clusters <- FUN(sig)
 
     # Cleaning up
     full.ids <- rep(NA_integer_, nrow(tab))
-    full.ids[sig] <- clusters$id
-    clusters$id <- full.ids
+    full.ids[sig] <- clusters$ids
+    clusters$ids <- full.ids
     clusters$FDR <- out$FDR 
-    return(clusters)
+    clusters
 }
 
 .weightedFDR <- function(p, w) {
