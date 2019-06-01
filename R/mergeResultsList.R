43787e9f43a5e42427d876884fd2596cc17d289c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-05-31 23:09:26 -0700

    Deleted redundant wrapper definition.

--- R/mergeResultsList.R
+++ R/mergeResultsList.R
@@ -23,28 +23,3 @@ mergeResultsList <- function(ranges.list, tab.list, tol, equiweight=TRUE, get.be
     output
 }
 
-#' @export
-#' @importFrom S4Vectors DataFrame metadata<-
-#' @importFrom IRanges findOverlaps
-overlapResultsList <- function(ranges.list, tab.list, regions, equiweight=TRUE, get.best=TRUE, 
-    overlap.args=list(), combine.args=list(), best.args=list()) 
-{
-    olap.out <- do.call(findOverlapsList, c(list(ranges.list, regions), overlap.args))
-    tab <- do.call(rbind, tab.list)
-
-    combine.args$olap <- best.args$olap <- olap.out$overlap
-    combine.args$tab <- best.args$tab <- tab
-    if (equiweight) {
-        combine.args$o.weights <- best.args$o.weights <- olap.out$weights
-    }
-
-    combined <- do.call(combineOverlaps, combine.args)
-    output <- DataFrame(regions=I(regions), combined=I(combined))
-    if (get.best) {
-        output$best <- do.call(getBestOverlaps, best.args)
-    }
-
-    metadata(output) <- olap.out
-    metadata(output)$tab <- tab
-    output
-}
