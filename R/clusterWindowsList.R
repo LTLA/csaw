9aeb19a68aa7d4c3292f421a96ae651f81ba77d9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-01 14:58:22 -0700

    Renamed consolidateClusters for consistency.

---
+++ R/clusterWindowsList.R
@@ -0,0 +1,46 @@
+#' @export
+clusterWindowsList <- function(ranges.list, tab.list, equiweight=TRUE, ...) 
+# This does the same as clusterWindows, but for results from many different analyses
+# (ostensibly with different window sizes).
+#
+# written by Aaron Lun
+# created 8 January 2016
+{
+    nset <- length(ranges.list)
+    set.it.vec <- seq_len(nset)
+    if (nset!=length(tab.list)) { stop("ranges.list must have same length as tab.list") }
+    
+    for (x in set.it.vec) {
+        ranges.list[[x]] <- .toGRanges(ranges.list[[x]])
+        currows <- length(ranges.list[[x]])
+        ntab <- nrow(tab.list[[x]])
+        if (currows!=ntab) { stop("corresponding entries of data and tab.lists must have same number of entries") }
+    }
+    
+    # Merging everyone together.
+    all.data <- do.call(c, ranges.list)
+    all.result <- do.call(rbind, tab.list)
+    groupings <- rep(seq_along(ranges.list), lengths(ranges.list)) 
+    
+    # Computing weights based on number of windows; each analysis contributes same effective number of tests.
+    # Not quite the same as consolidateWindows()'s weighting, but that can't be helped as the adjusted p-values \
+    # are computed before clustering.
+    if (equiweight) { 
+        weights <- rep(1/lengths(ranges.list), lengths(ranges.list))
+    } else {
+        weights <- NULL
+    }   
+
+    out <- clusterWindows(all.data, all.result, weights=weights, ...)
+    out$id <- split(out$id, groupings)
+    names(out$id) <- names(ranges.list)
+    return(out)
+}
+
+#' @export
+consolidateClusters <- function(data.list, result.list, equiweight=TRUE, ...) {
+    .Deprecated("clusterWindowsList")
+    clusterWindowsList(ranges.list=data.list, tab.list=result.list, equiweight=equiweight, ...)
+
+}
+
