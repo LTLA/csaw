7d92c9f10ac96bbd69b50e117a48edfebae58202
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-02 15:39:37 -0700

    Fixes to merge/overlapResults inputs, outputs - again.

--- R/mergeResults.R
+++ R/mergeResults.R
@@ -1,7 +1,7 @@
 #' @export
 #' @importFrom S4Vectors DataFrame
-mergeResults <- function(regions, tab, tol, get.best=TRUE, merge.args=list(), combine.args=list(), best.args=list()) {
-    merged <- do.call(mergeWindows, c(list(regions, tol=tol), merge.args))
+mergeResults <- function(ranges, tab, tol, get.best=TRUE, merge.args=list(), combine.args=list(), best.args=list()) {
+    merged <- do.call(mergeWindows, c(list(ranges, tol=tol), merge.args))
     combined <- do.call(combineTests, c(list(merged$id, tab), combine.args))
     output <- DataFrame(regions=I(merged$regions), combined=I(combined))
     if (get.best) {
--- R/overlapResults.R
+++ R/overlapResults.R
@@ -1,10 +1,10 @@
 #' @export
 #' @importFrom S4Vectors DataFrame
 #' @importFrom IRanges findOverlaps
-overlapResults <- function(regions, tab, ref, get.best=TRUE, overlap.args=list(), combine.args=list(), best.args=list()) {
-    olap <- do.call(findOverlaps, c(list(query=ref, subject=regions), overlap.args))
+overlapResults <- function(ranges, tab, regions, get.best=TRUE, overlap.args=list(), combine.args=list(), best.args=list()) {
+    olap <- do.call(findOverlaps, c(list(query=regions, subject=ranges), overlap.args))
     combined <- do.call(combineOverlaps, c(list(olap, tab), combine.args))
-    output <- DataFrame(regions=I(ref), combined=I(combined))
+    output <- DataFrame(regions=I(regions), combined=I(combined))
     if (get.best) {
         output$best <- do.call(getBestOverlaps, c(list(olap, tab), best.args))
     }
