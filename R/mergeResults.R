5f8dbf2ea9b705131c69705713921903e0ef4cc2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-02 11:15:06 -0700

    Fixed merge/overlapResults output names.

--- R/mergeResults.R
+++ R/mergeResults.R
@@ -3,7 +3,7 @@
 mergeResults <- function(regions, tab, tol, get.best=TRUE, merge.args=list(), combine.args=list(), best.args=list()) {
     merged <- do.call(mergeWindows, c(list(regions, tol=tol), merge.args))
     combined <- do.call(combineTests, c(list(merged$id, tab), combine.args))
-    output <- DataFrame(merged=I(merged$regions), combined=I(combined))
+    output <- DataFrame(regions=I(merged$regions), combined=I(combined))
     if (get.best) {
         output$best <- do.call(getBestTest, c(list(merged$id, tab), best.args))
     }
