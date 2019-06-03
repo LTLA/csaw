5f3f55a2b1e96fe0d0590c93d95371d43bcf7662
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-02 22:00:05 -0700

    Removed tests for deprecated consolidation functions. Added tests for new wrapper functions.

--- tests/testthat/test-cons.R
+++ tests/testthat/test-cons.R
@@ -7,18 +7,15 @@ set.seed(200000)
 test_that("mergeWindowsList works correctly", {
     for (nwin in c(5, 50, 500)) {
         for (sizes in list(c(10, 50), c(50, 50), c(100, 50))) {
-	        data.list <- list()
-        	for (s in seq_along(sizes)) {
-        		windows <- generateWindows(chromos, winsize=sizes[s], nwin=nwin)
-        		counts <- matrix(0, ncol=1, nrow=length(windows))
-        		data.list[[s]] <- SummarizedExperiment(list(counts=counts), rowRanges=windows)
-        	}
+            all.win <- list()
+            for (s in seq_along(sizes)) {
+                all.win[[s]] <- generateWindows(chromos, winsize=sizes[s], nwin=nwin)
+            }
 
-            all.win <- lapply(data.list, rowRanges)
             combined.win <- do.call(c, all.win)
             ref <- mergeWindows(combined.win, tol=100)
 
-        	cons <- mergeWindowsList(data.list, tol=100)
+            cons <- mergeWindowsList(all.win, tol=100)
             expect_identical(cons$ids, ref$ids)
             expect_identical(cons$regions, ref$regions)
             expect_true(all(combined.win==cons$ranges))
@@ -34,12 +31,9 @@ test_that("mergeWindowsList works correctly", {
             expect_equal(as.numeric(summed), rep(1, length(unique(f))))
 
             # Responds to the sign.
-            sign.list <- list()
-            for (s in seq_along(sizes)) {
-                sign.list[[s]] <- rbinom(length(data.list[[s]]), 1, 0.5)==1
-            }
-            cons3 <- mergeWindowsList(data.list, tol=100, signs=unlist(sign.list))
-            ref2 <- mergeWindows(combined.win, tol=100, signs=unlist(sign.list))
+            signs <- rbinom(length(combined.win), 1, 0.5)==1L
+            cons3 <- mergeWindowsList(all.win, tol=100, signs=signs)
+            ref2 <- mergeWindows(combined.win, tol=100, signs=signs)
             expect_identical(cons3$ids, ref2$ids)
             expect_identical(cons3$regions, ref2$regions)
         }
@@ -66,19 +60,16 @@ set.seed(200001)
 test_that("findOverlapsList works correctly", {
     for (nwin in c(5, 50, 500)) {
         for (sizes in list(c(10, 50), c(50, 50), c(100, 50))) {
-	    	data.list <- list()
-	        regions <- generateWindows(chromos, winsize=1000, nwin=5)
+            all.win <- list()
+            regions <- generateWindows(chromos, winsize=1000, nwin=5)
             for (s in seq_along(sizes)) {
-	        	windows <- generateWindows(chromos, winsize=sizes[s], nwin=nwin)
-	        	counts <- matrix(0, ncol=1, nrow=length(windows))
-	        	data.list[[s]] <- SummarizedExperiment(list(counts=counts), rowRanges=windows)
-	        }
+                all.win[[s]] <- generateWindows(chromos, winsize=sizes[s], nwin=nwin)
+            }
 
-            all.win <- lapply(data.list, rowRanges)
             combined.win <- do.call(c, all.win)
             ref <- findOverlaps(regions, combined.win)
 
-	        cons <- findOverlapsList(data.list, regions)
+            cons <- findOverlapsList(all.win, regions)
             expect_identical(cons$overlaps, ref)
             expect_true(all(combined.win==cons$ranges))
 
@@ -88,7 +79,7 @@ test_that("findOverlapsList works correctly", {
 
             # Checking that we respond to arguments.
             ref <- findOverlaps(regions, combined.win, type="within")
-            cons2 <- findOverlapsList(data.list, regions, type="within")
+            cons2 <- findOverlapsList(all.win, regions, type="within")
             expect_identical(cons2$overlaps, ref)
             expect_identical(cons$ranges, cons2$ranges)
 
@@ -110,123 +101,4 @@ test_that("findOverlapsList works correctly", {
     cons.E <- findOverlapsList(list(all.win[[1]][0], all.win[[2]][0]), regions)
     expect_identical(length(cons.E$overlaps), 0L)
     expect_identical(cons.E$weights, numeric(0))
-})
-
-set.seed(200002)
-test_that("consolidateTests works correctly", {
-    for (sizes in list(c(10, 50), c(50, 50), c(100, 50))) {
-        id.list <- result.list <- weight.list <- list()
-        for (s in seq_along(sizes)) {
-            ns <- sizes[s]
-            result.list[[s]] <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
-            id.list[[s]] <- sample(20, ns, replace=TRUE)
-            weight.list[[s]] <- runif(ns)
-        }
-
-        # Combining things for easier calls.
-        combo.id <- unlist(id.list)
-        combo.res <- do.call(rbind, result.list)
-        combo.weight <- unlist(weight.list)
-
-        # Running consolidation.
-        cons <- consolidateTests(id.list, result.list, weight.list=weight.list)
-        expect_identical(cons, combineTests(combo.id, combo.res, weight=combo.weight))
-
-        cons <- consolidateTests(id.list, result.list, weight.list=NULL)
-        expect_identical(cons, combineTests(combo.id, combo.res))
-
-        expect_warning(cons2 <- consolidateTests(id.list, result.list), "should be specified")
-        expect_identical(cons2, cons)
-
-        # Alternative function works, with proper re-indexing if necessary.
-        ref <- getBestTest(combo.id, combo.res, weight=combo.weight)
-        cons <- consolidateTests(id.list, result.list, weight.list=weight.list, FUN=getBestTest, reindex=NULL)
-        expect_identical(ref, cons)
-
-        cons <- consolidateTests(id.list, result.list, weight.list=weight.list, FUN=getBestTest)
-        expect_identical(ref$best, cons$best$row + c(0L, as.integer(sizes[1]))[cons$best$origin])
-        cons$best <- ref$best <- NULL
-        expect_identical(ref, cons)
-
-        cons <- consolidateTests(id.list, result.list, weight.list=NULL, FUN=getBestTest)
-        ref <- getBestTest(combo.id, combo.res)
-        expect_identical(ref$best, cons$best$row + c(0L, as.integer(sizes[1]))[cons$best$origin])
-        cons$best <- ref$best <- NULL
-        expect_identical(ref, cons)
-
-        # Argument specification works.
-        cons <- consolidateTests(id.list, result.list, weight.list=NULL, fc.col="logCPM")
-        expect_identical(cons, combineTests(combo.id, combo.res, fc.col="logCPM"))
-    }
-
-    # Testing all the error conditions.
-    expect_error(consolidateTests(id.list[1], result.list), "should be the same")
-    expect_error(consolidateTests(list(integer(0), integer(0)), result.list), "should match")
-    expect_error(consolidateTests(id.list[1], result.list, weight.list=weight.list[1]), "should be the same")
-    expect_error(consolidateTests(id.list, result.list, weight.list=list(integer(0), integer(0))), "should match")
-})
-
-set.seed(200003)
-test_that("consolidateOverlaps works correctly", {
-   for (np in c(5, 50, 500)) {
-        for (sizes in list(c(10, 50), c(50, 50), c(100, 50))) {
-        	olap.list <- result.list <- weight.list <- list()
-        	for (s in seq_along(sizes)) {
-                ns <- sizes[s]
-        		result.list[[s]] <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
-        		olap.list[[s]] <- Hits(sample(20, np, replace=TRUE), sample(ns, np, replace=TRUE),
-                        nLnode=25, # deliberately chosne to be >20, i.e., some regions have no overlapping windows.
-                        nRnode=ns, sort.by.query=TRUE)
-                weight.list[[s]] <- runif(np)
-            }
-
-            # Combining the Hits.
-            last <- 0
-            all.S <- vector("list", length(sizes))
-            for (s in seq_along(sizes)) {
-                all.S[[s]] <- subjectHits(olap.list[[s]]) + last
-                last <- last + nRnode(olap.list[[s]])
-            }
-            combo.olap <- Hits(unlist(lapply(olap.list, queryHits)), unlist(all.S),
-                nLnode=nLnode(olap.list[[s]]), nRnode=last, sort.by.query=TRUE)
-            combo.res <- do.call(rbind, result.list)
-            combo.weight <- unlist(weight.list)
-
-            # Running consolidation.
-            cons <- consolidateOverlaps(olap.list, result.list, weight.list=weight.list)
-            expect_identical(cons, combineOverlaps(combo.olap, combo.res, o.weight=combo.weight))
-
-            cons <- consolidateOverlaps(olap.list, result.list, weight.list=NULL)
-            expect_identical(cons, combineOverlaps(combo.olap, combo.res))
-
-            expect_warning(cons2 <- consolidateOverlaps(olap.list, result.list), "should be specified")
-            expect_identical(cons2, cons)
-
-            # Alternative function works, with proper re-indexing if necessary.
-            ref <- getBestOverlaps(combo.olap, combo.res, o.weight=combo.weight)
-            cons <- consolidateOverlaps(olap.list, result.list, weight.list=weight.list, FUN=getBestOverlaps, reindex=NULL)
-            expect_identical(ref, cons)
-            
-            cons <- consolidateOverlaps(olap.list, result.list, weight.list=weight.list, FUN=getBestOverlaps)
-            expect_identical(ref$best, cons$best$row + c(0L, as.integer(sizes))[cons$best$origin])
-            cons$best <- ref$best <- NULL
-            expect_identical(ref, cons)
-            
-            ref <- getBestOverlaps(combo.olap, combo.res)
-            cons <- consolidateOverlaps(olap.list, result.list, weight.list=NULL, FUN=getBestOverlaps)
-            expect_identical(ref$best, cons$best$row + c(0L, as.integer(sizes))[cons$best$origin])
-            cons$best <- ref$best <- NULL
-            expect_identical(ref, cons)
-
-            # Argument specification works.
-            cons <- consolidateOverlaps(olap.list, result.list, weight.list=NULL, fc.col="logCPM")
-            expect_identical(cons, combineOverlaps(combo.olap, combo.res, fc.col="logCPM"))
-        }
-   }
-
-    # Testing all the error conditions.
-    expect_error(consolidateOverlaps(olap.list[1], result.list), "should be the same")
-    expect_error(consolidateOverlaps(olap.list, list(result.list[[1]][0,], result.list[[2]])), "different length")
-    expect_error(consolidateOverlaps(olap.list[1], result.list, weight.list=weight.list[1]), "should be the same")
-    expect_error(consolidateOverlaps(olap.list, result.list, weight.list=list(integer(0), integer(0))), "should match")
 })
---
+++ tests/testthat/test-wrappers.R
@@ -0,0 +1,152 @@
+# This tests the various wrapper functions.
+# library(testthat); library(csaw); source("setup.R"); source("test-wrappers.R")
+
+set.seed(1919000)
+test_that("mergeResults works correctly", {
+    chromos <- c(chrA=10000, chrB=5000, chrC=2000)
+    reg <- generateWindows(chromos, nwin=100, winsize=50)
+    ns <- length(reg)
+    tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
+
+    # Yields all output fields, correctly named.
+    merged <- mergeResults(reg, tab, tol=100)
+    expect_s4_class(merged$regions, "GRanges")
+    expect_s4_class(merged$combined, "DataFrame")
+    expect_s4_class(merged$best, "DataFrame")
+    expect_type(metadata(merged)$ids, "integer")
+
+    # Responds to other arguments
+    merged2 <- mergeResults(reg, tab, tol=1)
+    expect_false(identical(merged, merged2))
+
+    merged2 <- mergeResults(reg, tab, tol=100, merge.args=list(max.width=200))
+    expect_false(identical(merged, merged2))
+
+    merged2 <- mergeResults(reg, tab, tol=100, combine.args=list(fc.col="logCPM"))
+    expect_false(identical(merged, merged2))
+
+    merged2 <- mergeResults(reg, tab, tol=100, get.best=FALSE)
+    expect_false(identical(merged, merged2))
+
+    # Behaves with empty arguments.
+    expect_identical(nrow(mergeResults(reg[0], tab[0,], tol=10)), 0L)
+})
+
+set.seed(1919001)
+test_that("overlapResults works correctly", {
+    chromos <- c(chrA=10000, chrB=5000, chrC=2000)
+    reg <- generateWindows(chromos, nwin=100, winsize=50)
+    ref <- generateWindows(chromos, nwin=5, winsize=50)
+    ns <- length(reg)
+    tab <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
+
+    # Yields all output fields, correctly named.
+    merged <- overlapResults(reg, tab, regions=ref)
+    expect_identical(nrow(merged), length(ref))
+
+    expect_s4_class(merged$regions, "GRanges")
+    expect_s4_class(merged$combined, "DataFrame")
+    expect_s4_class(merged$best, "DataFrame")
+    expect_s4_class(metadata(merged)$overlaps, "Hits")
+
+    # Responds to other arguments
+    merged2 <- overlapResults(reg, tab, regions=ref, overlap.args=list(type="within"))
+    expect_false(identical(merged, merged2))
+
+    merged2 <- overlapResults(reg, tab, regions=ref, combine.args=list(fc.col="logCPM"))
+    expect_false(identical(merged, merged2))
+
+    merged2 <- overlapResults(reg, tab, regions=ref, get.best=FALSE)
+    expect_false(identical(merged, merged2))
+
+    # Behaves with empty arguments.
+    expect_identical(nrow(overlapResults(reg[0], tab[0,], ref)), length(ref))
+})
+
+set.seed(1919002)
+test_that("mergeResultsList works correctly", {
+    data.list <- result.list <- list()
+    chromos <- c(chrA=10000, chrB=5000, chrC=2000)
+    sizes <- c(10, 20, 50)
+    for (s in seq_along(sizes)) {
+        data.list[[s]] <- generateWindows(chromos, winsize=sizes[s], nwin=20)
+        ns <- length(data.list[[s]])
+        result.list[[s]] <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
+    }
+
+    merged <- mergeResultsList(data.list, result.list, tol=100)
+    expect_s4_class(merged$regions, "GRanges")
+    expect_s4_class(merged$combined, "DataFrame")
+    expect_s4_class(merged$best, "DataFrame")
+
+    expect_type(metadata(merged)$ids, "integer")
+    expect_type(metadata(merged)$weights, "double")
+    expect_identical(length(metadata(merged)$ranges), sum(lengths(data.list)))
+    expect_identical(metadata(merged)$tab, do.call(rbind, result.list))
+
+    # Responds to other arguments.
+    merged2 <- mergeResultsList(data.list, result.list, tol=1)
+    expect_false(identical(merged, merged2))
+
+    merged2 <- mergeResultsList(data.list, result.list, tol=100, merge.args=list(max.width=200))
+    expect_false(identical(merged, merged2))
+
+    merged2 <- mergeResultsList(data.list, result.list, tol=100, combine.args=list(fc.col="logCPM"))
+    expect_false(identical(merged, merged2))
+
+    merged2 <- mergeResultsList(data.list, result.list, tol=100, get.best=FALSE)
+    expect_false(identical(merged, merged2))
+
+    merged2 <- mergeResultsList(data.list, result.list, tol=100, equiweight=FALSE)
+    expect_false(identical(merged, merged2))
+
+    # Behaves with single arguments.
+    merged3 <- mergeResultsList(data.list[1], result.list[1], tol=100)
+    refout <- mergeResults(data.list[[1]], result.list[[1]], tol=100)
+    expect_equal(merged3$combined, refout$combined)
+    expect_equal(merged3$best, refout$best)
+    expect_true(all(merged3$regions==refout$regions))
+})
+
+set.seed(1919003)
+test_that("overlapResultsList works correctly", {
+    data.list <- result.list <- list()
+    chromos <- c(chrA=10000, chrB=5000, chrC=2000)
+    ref <- generateWindows(chromos, nwin=5, winsize=50)
+    sizes <- c(10, 20, 50)
+    for (s in seq_along(sizes)) {
+        data.list[[s]] <- generateWindows(chromos, winsize=sizes[s], nwin=20)
+        ns <- length(data.list[[s]])
+        result.list[[s]] <- data.frame(logFC=rnorm(ns), PValue=rbeta(ns, 1, 3), logCPM=rnorm(ns))
+    }
+
+    merged <- mergeResultsList(data.list, result.list, tol=100)
+    expect_s4_class(merged$regions, "GRanges")
+    expect_s4_class(merged$combined, "DataFrame")
+    expect_s4_class(merged$best, "DataFrame")
+
+    expect_type(metadata(merged)$ids, "integer")
+    expect_type(metadata(merged)$weights, "double")
+    expect_identical(length(metadata(merged)$ranges), sum(lengths(data.list)))
+    expect_identical(metadata(merged)$tab, do.call(rbind, result.list))
+
+    # Responds to other arguments.
+    merged2 <- overlapResultsList(data.list, result.list, regions=ref, overlap.args=list(type="within"))
+    expect_false(identical(merged, merged2))
+
+    merged2 <- overlapResultsList(data.list, result.list, regions=ref, combine.args=list(fc.col="logCPM"))
+    expect_false(identical(merged, merged2))
+
+    merged2 <- overlapResultsList(data.list, result.list, regions=ref, get.best=FALSE)
+    expect_false(identical(merged, merged2))
+
+    merged2 <- overlapResultsList(data.list, result.list, regions=ref, equiweight=FALSE)
+    expect_false(identical(merged, merged2))
+
+    # Behaves with single arguments.
+    merged3 <- overlapResultsList(data.list[1], result.list[1], regions=ref)
+    refout <- overlapResults(data.list[[1]], result.list[[1]], regions=ref)
+    expect_equal(merged3$combined, refout$combined)
+    expect_equal(merged3$best, refout$best)
+    expect_true(all(merged3$regions==refout$regions))
+})
